d11df1019e2dcd2e51d5ee601f9d54ae12c98121
Jenifer Kovács <fenicser85@gmail.com>
2020-08-25 18:58:44 +0200

    Changes.

---
+++ adotanacsadas/src/main/java/hu/beni/adotanacsadas/AdotanacsadasApplication.java
@@ -0,0 +1,13 @@
+package hu.beni.adotanacsadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdotanacsadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdotanacsadasApplication.class, args);
+	}
+
+}
---
+++ adotanacsadas/src/test/java/hu/beni/adotanacsadas/AdotanacsadasApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.beni.adotanacsadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotanacsadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
