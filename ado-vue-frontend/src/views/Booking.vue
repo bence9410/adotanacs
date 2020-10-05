1adac0fa8845bc06a9cfa08ebf795b7fa1fad4b3
Jenifer Kovács <fenicser85@gmail.com>
2020-10-05 18:08:05 +0200

    Create dialog in  booking.

--- ado-vue-frontend/src/views/Booking.vue
+++ ado-vue-frontend/src/views/Booking.vue
@@ -36,25 +36,105 @@
         outlined
         class="ma-2"
         v-for="time in ft.times"
-        :key="time"
+        :key="time.time"
       >
         <v-row class="mt-3">
           <v-col cols="6">
-            <v-card-text class="pa-3">{{ time }}-tól</v-card-text>
+            <v-card-text class="pa-3">{{ time.time }}-tól</v-card-text>
           </v-col>
           <v-col cols="5 " class="pb-0">
-            <v-select dense solo class="pa-0" label="30 perc"> </v-select>
+            <v-select
+              dense
+              solo
+              class="pa-0"
+              label="30 perc"
+              :items="interval"
+              v-model="time.interval"
+            >
+            </v-select>
           </v-col>
         </v-row>
-        <v-col class="pt-0">
-          <v-btn elevation="2" block dark color="green " class="mx-auto"
+        <v-col class="pt-0"
+          ><v-btn
+            elevation="2"
+            block
+            dark
+            color="green "
+            class=""
+            @click="openDialog(ft.date, time)"
             >Lefoglalom
           </v-btn>
         </v-col>
       </v-card>
     </v-card>
+    <v-dialog v-model="dialog.show" persistent max-width="600px">
+      <v-card>
+        <div class="text-right" style="width: 100%">
+          <v-btn icon @click="dialog.show = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </div>
+        <v-card-title>
+          <span class="headline"
+            >Kérem töltse ki a foglaláshoz az adatait.<br />{{
+              dialog.title + dialog.time
+            }}</span
+          >
+        </v-card-title>
 
-    <div id="avaiableTimes"></div>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="Teljes név*"
+                  required
+                  v-model="dialog.data.name"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Email cím*"
+                  required
+                  v-model="dialog.data.email"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-select
+                  :items="['Skype', 'Személyes']"
+                  label="Találkozó módja*"
+                  required
+                  v-model="dialog.data.meetingType"
+                ></v-select>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea
+                  outlined
+                  autocomplete="email"
+                  label="Rövid leírás*"
+                  placeholder="Kérem röviden írja le milyen ügyben keresett fel."
+                  v-model="dialog.data.description"
+                ></v-textarea>
+                <v-checkbox
+                  v-model="ex4"
+                  label="Hozzájárulok, hogy a megadott adataimat megőrizzék és felvegyék velem a kapcsolatot.*"
+                  color="info"
+                  value="dataManagement"
+                  class="mt-0"
+                  hide-details
+                ></v-checkbox>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" dark @click="dialog.show = false">
+            Lefoglalom
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
@@ -67,27 +147,54 @@ export default {
     freeTimes: {},
     availableTimesShown: ["14:30", "16:00", "17:30"],
     availableTimes: ["PM2_30", "PM4_00", "PM5_30"],
+    interval: ["30 perc", "60 perc"],
+
+    freeTimesShown: [],
+    dialog: {
+      show: false,
+      data: {
+        ///mezonevek amik az entitáson vannak a:
+        name: "",
+        email: "",
+        description: "",
+        meetingTime: "",
+        meetingLenght: "",
+        meetingType: "",
+      },
+      title: "",
+      date: "",
+      time: "",
+    },
   }),
-  computed: {
-    freeTimesShown() {
-      var ft = [];
-      $.each(this.freeTimes, (key, value) => {
-        var av = [];
-        for (let i = 0; i < value.length; i++) {
-          av.push(
-            this.availableTimesShown[this.availableTimes.indexOf(value[i])]
-          );
-        }
-        ft.push({ date: key.replaceAll("-", ".") + ".", times: av });
-      });
-      return ft;
+  methods: {
+    openDialog(date, time) {
+      this.dialog.show = true;
+      this.dialog.title = date + time.time + " " + time.interval;
     },
   },
 
   created() {
     $.ajax({
       url: "/api/free-times",
-      success: (data) => (this.freeTimes = data),
+      success: (data) => {
+        this.freeTimes = data;
+
+        $.each(this.freeTimes, (key, value) => {
+          var av = [];
+          for (let i = 0; i < value.length; i++) {
+            av.push({
+              time: this.availableTimesShown[
+                this.availableTimes.indexOf(value[i])
+              ],
+              interval: "30 perc",
+            });
+          }
+          this.freeTimesShown.push({
+            date: key.replaceAll("-", ".") + ".",
+            times: av,
+          });
+        });
+      },
     });
   },
 };
