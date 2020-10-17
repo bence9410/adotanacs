a9629e1be0ea7e18e11c7d027a66d973a9b51bf8
Bence Németh <nembence1994@gmail.com>
2020-10-17 17:02:39 +0200

    Add prodBuild.sh, builds frontend, copies dist to backend public, builds backend. gitignore backend public.

---
+++ prodBuild.sh
@@ -0,0 +1,5 @@
+#email user and pass is missing from application.yml, do not commit it to public repository!
+cd tax-vue-frontend; npm run build; cd ..;
+rm -r -f tax-spring-backend/src/main/resources/public/*;
+cp -r tax-vue-frontend/dist/* tax-spring-backend/src/main/resources/public/;
+cd tax-spring-backend; mvn clean package -DskipTests;
