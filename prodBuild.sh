160ce723f3201ec3a0803869e9b9a30f3cbb5d7b
Jenifer <fenicser85@gmail.com>
2020-10-19 14:53:31 +0200

    Build vue, add mkdir to prodBuild.sh.

--- prodBuild.sh
+++ prodBuild.sh
@@ -1,5 +1,6 @@
 #email user and pass is missing from application.yml, do not commit it to public repository!
 cd tax-vue-frontend; npm run build; cd ..;
 rm -r -f tax-spring-backend/src/main/resources/public/*;
+mkdir tax-spring-backend/src/main/resources/public/
 cp -r tax-vue-frontend/dist/* tax-spring-backend/src/main/resources/public/;
 cd tax-spring-backend; mvn clean package -DskipTests;
