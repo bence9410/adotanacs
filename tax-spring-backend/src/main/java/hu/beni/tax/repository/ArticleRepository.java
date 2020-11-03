a334c3a196c793678867edde7f2286edf436840d
Jenifer <fenicser85@gmail.com>
2020-11-03 11:05:18 +0100

    Order articles.

--- tax-spring-backend/src/main/java/hu/beni/tax/repository/ArticleRepository.java
+++ tax-spring-backend/src/main/java/hu/beni/tax/repository/ArticleRepository.java
@@ -1,9 +1,13 @@
 package hu.beni.tax.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hu.beni.tax.entity.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findAllByOrderByIdDesc();
 
 }
